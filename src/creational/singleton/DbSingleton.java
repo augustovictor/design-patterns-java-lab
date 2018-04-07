d3267fcb07e5b8fd1838fb18c43caba02bda9b9b
Victor <victoraweb@gmail.com>
2018-04-07 17:11:03 -0300

    [FEAT] Thread safe

--- src/creational/singleton/DbSingleton.java
+++ src/creational/singleton/DbSingleton.java
@@ -1,13 +1,21 @@
 package creational.singleton;
 
 public class DbSingleton {
-    private static DbSingleton instance = null;
+    private static volatile DbSingleton instance = null;
 
-    private DbSingleton() {}
+    private DbSingleton() {
+        if (instance != null) {
+            throw new RuntimeException("Use getInstance() method to create a DbSingleton instance");
+        }
+    }
 
     public static DbSingleton getInstance() {
         if (instance == null) {
-            instance = new DbSingleton();
+            synchronized (DbSingleton.class) {
+                if(instance == null) {
+                    instance = new DbSingleton();
+                }
+            }
         }
 
         return instance;
